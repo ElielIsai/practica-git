42e1f915e27964748c25205174d95abb877d874e
ElielISai <20243rd049@utez.edu.mx>
2025-09-17 16:09:24 -0600

    busqueda de repos y da mensaje si no los hay

--- practicaScaner.py
+++ practicaScaner.py
@@ -9,3 +9,11 @@ git_dir = r"C:\Users\CC6\Desktop\practicagit"
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir, name)) and os.path.exists(os.path.join(git_dir, name, ".git"))]
 
+if repos:
+    print("Found following Github repositories in C:\Users\CC6\Desktop\practicagit")
+    for idx, repo in enumerate(repos,1):
+        print(f"{idx},{repo}")
+else:
+    print("No Github repositories found in C:\Users\CC6\Desktop\practicagit")
+    print("Please provide a Github repositories") 
+    
